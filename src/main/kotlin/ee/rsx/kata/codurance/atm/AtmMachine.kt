80a1a4898eba06da03dcd8384c774b168f88419f
Risto Uibo <risto@rsx.ee>
2024-01-31 13:50:49 +0200

    kata | Codurance | ATM machine: iteration 2 - implement ATM with limited funds

--- src/main/kotlin/ee/rsx/kata/codurance/atm/AtmMachine.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/atm/AtmMachine.kt
@@ -1,10 +1,51 @@
 package ee.rsx.kata.codurance.atm
 
 import ee.rsx.kata.codurance.atm.money.Note
+import ee.rsx.kata.codurance.atm.money.Note.*
+import kotlin.math.min
 
-class AtmMachine {
+class AtmMachine(limitedFunds: Boolean = false) {
+
+    private val funds: MutableList<Note>?
+
+    init {
+        funds = if (limitedFunds)
+            filledFunds()
+        else
+            null
+    }
+
+    private fun filledFunds(): MutableList<Note> {
+        val funds = mutableListOf<Note>()
+        repeat(2) { funds.add(BILL_500) }
+        repeat(3) { funds.add(BILL_200) }
+        repeat(5) { funds.add(BILL_100) }
+        repeat(12) { funds.add(BILL_50) }
+        repeat(20) { funds.add(BILL_20) }
+        repeat(50) { funds.add(BILL_10) }
+        repeat(100) { funds.add(BILL_5) }
+        repeat(250) { funds.add(COIN_2) }
+        repeat(500) { funds.add(COIN_1) }
+        funds.sortByDescending { it.nomination }
+        return funds
+    }
+
+    val remainingBalance: Int
+        get() = funds?.let { funds.sumOf { it.nomination } }
+            ?: throw IllegalStateException("Cannot determine remaining balance, ATM works with limitless funds")
+
+    val remainingFunds: List<Note>
+        get() = funds?.let { funds.toList() }
+            ?: throw IllegalStateException("Cannot determine remaining funds, ATM works with limitless funds")
 
     fun withdraw(amount: Int): List<Note> {
+        return if (funds != null) {
+            withdrawFromFunds(amount)
+        } else
+            withdrawLimitless(amount)
+    }
+
+    private fun withdrawLimitless(amount: Int): List<Note> {
         var remainingAmount = amount
 
         return Note.entries
@@ -12,8 +53,8 @@ class AtmMachine {
             .flatMap { note ->
                 val modulus = remainingAmount % note.nomination
 
-                if (modulus == remainingAmount)
-                    emptyList()
+                if (modulus == remainingAmount){
+                    emptyList()}
                 else {
                     val sumToReduce = remainingAmount - modulus
                     remainingAmount -= sumToReduce
@@ -22,5 +63,48 @@ class AtmMachine {
                     List(notesCount) { note }
                 }
             }
+
+    }
+
+    private fun withdrawFromFunds(amount: Int): List<Note> {
+        var remainingAmount = amount
+
+        requireNotNull(funds)
+
+        val notesTaken = Note.entries
+            .sortedByDescending { it.nomination }
+            .flatMap { note ->
+                val modulus = remainingAmount % note.nomination
+
+                if (remainingAmount == 0 || modulus == remainingAmount || funds.none { it == note })
+                    emptyList()
+                else {
+                    val sumOfNotes = remainingAmount - modulus
+                    val requiredNotesCount = sumOfNotes / note.nomination
+
+                    val availableNotes = funds.countOf(note)
+                    val howManyToTake = min(availableNotes, requiredNotesCount)
+
+                    val sumToReduce = howManyToTake * note.nomination
+                    remainingAmount -= sumToReduce
+
+                    funds.take(howManyToTake, note)
+                }
+            }
+
+        // TODO if remaining amount is not 0, then we don't have enough of suitable notes - put them back and throw error
+
+        return notesTaken
+    }
+
+    private fun List<Note>.countOf(note: Note) = count { it == note }
+
+    private fun MutableList<Note>.take(times: Int, note: Note): List<Note> {
+        val notesTaken: MutableList<Note> = mutableListOf()
+        repeat(times) {
+            val removeIndex = indexOfFirst { availableNote -> availableNote == note }
+            notesTaken.add(removeAt(removeIndex))
+        }
+        return notesTaken
     }
 }
--- src/test/kotlin/ee/rsx/kata/codurance/atm/AtmMachineTest.kt
+++ src/test/kotlin/ee/rsx/kata/codurance/atm/AtmMachineTest.kt
@@ -1,5 +1,6 @@
 package ee.rsx.kata.codurance.atm
 
+import ee.rsx.kata.codurance.atm.money.Note
 import ee.rsx.kata.codurance.atm.money.Note.*
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.Test
@@ -44,4 +45,77 @@ class AtmMachineTest {
                 COIN_2,
             )
     }
+
+    @Test
+    fun `withdraw funds of 1725 Euros, from limited funds of ATM`() {
+        val atm = AtmMachine(limitedFunds = true)
+        val initialBalance = atm.remainingBalance
+
+        val withdrawal = atm.withdraw(1725)
+
+        assertThat(withdrawal.sumOf { it.nomination }).isEqualTo(1725)
+        assertThat(withdrawal)
+            .containsExactlyInAnyOrder(
+                BILL_500,
+                BILL_500,
+                BILL_200,
+                BILL_200,
+                BILL_200,
+                BILL_100,
+                BILL_20,
+                BILL_5
+            )
+
+        val expectedRemainingBalance = initialBalance - 1725
+        assertThat(atm.remainingBalance).isEqualTo(expectedRemainingBalance)
+
+        val remainingFunds = atm.remainingFunds
+        remainingFunds.assertHas(0, BILL_500)
+        remainingFunds.assertHas(0, BILL_200)
+        remainingFunds.assertHas(4, BILL_100)
+        remainingFunds.assertHas(12, BILL_50)
+        remainingFunds.assertHas(19, BILL_20)
+        remainingFunds.assertHas(50, BILL_10)
+        remainingFunds.assertHas(99, BILL_5)
+        remainingFunds.assertHas(250, COIN_2)
+        remainingFunds.assertHas(500, COIN_1)
+    }
+
+    @Test
+    fun `withdraw funds of 1725 Euros and then 1825 Euros, from limited funds of ATM`() {
+        val atm = AtmMachine(limitedFunds = true)
+        val initialBalance = atm.remainingBalance
+
+        atm.withdraw(1725)
+        val withdrawal = atm.withdraw(1825)
+
+        assertThat(withdrawal.sumOf { it.nomination }).isEqualTo(1825)
+
+        val expectedWithdrawal = mutableListOf<Note>()
+        repeat(4) { expectedWithdrawal.add(BILL_100) }
+        repeat(12) { expectedWithdrawal.add(BILL_50) }
+        repeat(19) { expectedWithdrawal.add(BILL_20) }
+        repeat(44) { expectedWithdrawal.add(BILL_10) }
+        repeat(1) { expectedWithdrawal.add(BILL_5) }
+        assertThat(withdrawal)
+            .containsExactlyInAnyOrderElementsOf(expectedWithdrawal)
+
+        val expectedRemainingBalance = initialBalance - 1725 - 1825
+        assertThat(atm.remainingBalance)
+            .isEqualTo(expectedRemainingBalance)
+
+        val remainingFunds = atm.remainingFunds
+        remainingFunds.assertHas(0, BILL_500)
+        remainingFunds.assertHas(0, BILL_200)
+        remainingFunds.assertHas(0, BILL_100)
+        remainingFunds.assertHas(0, BILL_50)
+        remainingFunds.assertHas(0, BILL_20)
+        remainingFunds.assertHas(6, BILL_10)
+        remainingFunds.assertHas(98, BILL_5)
+        remainingFunds.assertHas(250, COIN_2)
+        remainingFunds.assertHas(500, COIN_1)
+    }
+
+
+    private fun List<Note>.assertHas(countOf: Int, note: Note) = assertThat(count { it == note }).isEqualTo(countOf)
 }
