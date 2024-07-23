db66a9ba2ff5158cb39f9cc7dd010e024c6817e6
Risto Uibo <risto@rsx.ee>
2024-07-23 13:48:09 +0300

    remove Shopping Cart kata (separated to own repo)

--- src/main/kotlin/ee/rsx/kata/codurance/shoppingcart/cart/CartItem.kt
+++
@@ -1,5 +0,0 @@
-package ee.rsx.kata.codurance.shoppingcart.cart
-
-import ee.rsx.kata.codurance.shoppingcart.product.Product
-
-data class CartItem(val product: Product)
--- src/main/kotlin/ee/rsx/kata/codurance/shoppingcart/rate/TaxRate.kt
+++
@@ -1,6 +0,0 @@
-package ee.rsx.kata.codurance.shoppingcart.rate
-
-enum class TaxRate(val percentage: Percentage) {
-  NORMAL(Percentage(rate = 21)),
-  FIRST_NECESSITY(Percentage(rate = 10))
-}
